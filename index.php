b624755e00af75218e6c9ec085a798ad672b2998
Sage Markwardt <sagemarkwardt@gmail.com>
2024-04-18 12:33:09 -0400

    completed order1 route

--- index.php
+++ index.php
@@ -41,17 +41,47 @@ $f3->route('GET /menus/dinner', function (){
 });
 
 // define the route for the order menu
-$f3->route('GET /order1', function (){
+$f3->route('GET|POST /order1', function ($f3){
+    //if the form has already been submitted
+    if ($_SERVER['REQUEST_METHOD'] == "POST"){
+        echo "This was post access.";
+
+        //get data from post array
+        $food = $_POST['food'];
+        $meal = $_POST['meal'];
+
+        // if data valid can say if(true) as well
+        // this way checks to make sure both values were at least selected
+        // basic validation
+        if (true){
+            // $f3 is not within the scope
+            // PHP has to be TOLD to look for global variables
+            // add the data to the session array
+            $f3->set("SESSION.food", $food);
+            $f3->set("SESSION.meal", $meal);
+
+            // if data is set and valid, send to step 2 of order form
+            $f3->reroute('order2');
+        } else {
+            // not great practice, temporary because this should be
+            // in our view pages not directly in php
+            echo "<p>validation errors</p>";
+        }
+    } else {
+        echo "This is get method access.";
+    }
+
     $view = new Template();
     // render the specific file we want
     echo $view->render('views/order1.html');
 });
 
 // define the route for the order menu
-$f3->route('GET /order2', function (){
+$f3->route('GET|POST /order2', function ($f3){
+    var_dump($f3->get('SESSION'));
     $view = new Template();
     // render the specific file we want
-    echo $view->render('views/order.html');
+    echo $view->render('views/order2.html');
 });
 
 // run fat free, otherwise the page won't load
